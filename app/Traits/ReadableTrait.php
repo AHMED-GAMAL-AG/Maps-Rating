ca38cda6f13898ff67c01e51abc5a7cce02fa92a
jimmy-glitch <76778937+AHMED-GAMAL-AG@users.noreply.github.com>
2023-05-04 16:14:05 +0300

    03. طريقة حساب معدل التقييم

---
+++ app/Traits/ReadableTrait.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Traits;
+
+trait ReadableTrait
+{
+    public function averageRating($place)
+    {
+        $avg = $place->reviews()
+            ->selectRaw('avg(service_rating)  service_rating, avg(quality_rating)  quality_rating,avg(cleanliness_rating) cleanliness_rating, avg(pricing_rating)  pricing_rating')
+            ->first();
+
+        $total = array_sum($avg->toArray()) / 4;
+
+        return [
+            'total' => $total,
+            'service_rating' => $avg->service_rating,
+            'quality_rating' => $avg->quality_rating,
+            'cleanliness_rating' => $avg->cleanliness_rating,
+            'pricing_rating' => $avg->pricing_rating,
+        ];
+    }
+}
